d84d1dadd04ab3c2bb99d33443e2147548bae692
Stephen Rowe <rowes@tcd.ie>
2020-10-16 04:42:13 +0100

    all tests passed

--- src/Ex01.hs
+++ src/Ex01.hs
@@ -74,7 +74,7 @@ runs xs =
     let (_, remainderArr, groupArr) = innerArr ((head xs), (tail xs), [])
     in [groupArr] ++ runs remainderArr
 
--- returns a tuple - 'x' is the value used for comparison, 'xs' is the array being iterated, 'groupArr' is being built to make [1,1,1] or [2,2], etc
+-- helper function, returns a tuple - 'x' is the value used for comparison, 'xs' is the array being iterated, 'groupArr' is being built to make [1,1,1] or [2,2], etc
 innerArr :: (Eq a) => (a, [a], [a]) -> (a, [a], [a])
 innerArr (x, [], groupArr) = (x, [], x : groupArr)
 innerArr (x, xs, groupArr)
