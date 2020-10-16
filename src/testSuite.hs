94fa3c0c761d95cb983d6c4a7cfeaae90963b0a6
Stephen Rowe <rowes@tcd.ie>
2020-10-16 03:57:16 +0100

    finished a test() function, which returns a triple with (index of first differing value, value currently being used for comparison, remaining array after repeating elements removed)

--- src/testSuite.hs
+++ src/testSuite.hs
@@ -2,6 +2,9 @@ runs :: Eq a => [a] -> [[a]]
 runs [] = [[]]
 runs xs = [head xs : (test (head xs) (tail xs))] ++ runs (tail xs)
 
-test x xs
-    | x == head xs = head xs : test (head xs) (tail xs)
-    | otherwise = []
+test :: (Eq a) => (Int, a, [a]) -> (Int, a, [a])
+
+test (n, x, []) = (n, x, [])
+test (n, x, xs)
+    | x == (head xs) = test ((n+1), x, (tail xs))
+    | otherwise = (n, x, xs)
