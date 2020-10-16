f886d1807e7da80cefe360ac7aae7c127870db03
Stephen Rowe <rowes@tcd.ie>
2020-10-16 04:13:12 +0100

    getting a result for runs with the current version of test(), but will now adjust test to exclude some values in the parameter tuple to see if that helps

--- src/testSuite.hs
+++ src/testSuite.hs
@@ -1,10 +1,13 @@
 runs :: Eq a => [a] -> [[a]]
 runs [] = [[]]
-runs xs = [head xs : (test (head xs) (tail xs))] ++ runs (tail xs)
+-- runs xs = [head xs : (test (head xs) (tail xs))] ++ runs (tail xs)
 
-test :: (Eq a) => (Int, a, [a]) -> (Int, a, [a])
+runs xs =
+    let (index, comp, remainingArray, rA) = test (0, (head xs), (tail xs), [])
+    in [rA] ++ runs remainingArray
 
-test (n, x, []) = (n, x, [])
-test (n, x, xs)
-    | x == (head xs) = test ((n+1), x, (tail xs))
-    | otherwise = (n, x, xs)
+test :: (Eq a) => (Int, a, [a], [a]) -> (Int, a, [a], [a])
+test (n, x, [], rA) = (n, x, [], rA)
+test (n, x, xs, rA)
+    | x == (head xs) = test ((n+1), x, (tail xs), (x : rA))
+    | otherwise = (n, x, xs, rA)
