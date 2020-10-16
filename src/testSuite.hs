4e372b38511e50263907480dffcbca95411d4e14
Stephen Rowe <rowes@tcd.ie>
2020-10-16 04:20:02 +0100

    getting close to a correct result for runs(), but getting incorrect values for the tail end of the array

--- src/testSuite.hs
+++ src/testSuite.hs
@@ -2,12 +2,12 @@ runs :: Eq a => [a] -> [[a]]
 runs [] = [[]]
 -- runs xs = [head xs : (test (head xs) (tail xs))] ++ runs (tail xs)
 
--- runs xs =
---     let (index, comp, remainingArray, rA) = test (0, (head xs), (tail xs), [])
---     in [rA] ++ runs remainingArray
+runs xs =
+    let (comp, remainingArray, rA) = test ((head xs), (tail xs), [])
+    in [rA] ++ runs remainingArray
 
 test :: (Eq a) => (a, [a], [a]) -> (a, [a], [a])
 test (x, [], rA) = (x, [], rA)
 test (x, xs, rA)
     | x == (head xs) = test (x, (tail xs), (x : rA))
-    | otherwise = (x, xs, rA)
+    | otherwise = (x, xs, x : rA)
