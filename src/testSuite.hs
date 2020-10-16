8b3087592a9943dc2c09808e7a832746c8876675
Stephen Rowe <rowes@tcd.ie>
2020-10-16 04:16:21 +0100

    removed array index parameter from tuple for test(), shows no effect on the program

--- src/testSuite.hs
+++ src/testSuite.hs
@@ -2,12 +2,12 @@ runs :: Eq a => [a] -> [[a]]
 runs [] = [[]]
 -- runs xs = [head xs : (test (head xs) (tail xs))] ++ runs (tail xs)
 
-runs xs =
-    let (index, comp, remainingArray, rA) = test (0, (head xs), (tail xs), [])
-    in [rA] ++ runs remainingArray
+-- runs xs =
+--     let (index, comp, remainingArray, rA) = test (0, (head xs), (tail xs), [])
+--     in [rA] ++ runs remainingArray
 
-test :: (Eq a) => (Int, a, [a], [a]) -> (Int, a, [a], [a])
-test (n, x, [], rA) = (n, x, [], rA)
-test (n, x, xs, rA)
-    | x == (head xs) = test ((n+1), x, (tail xs), (x : rA))
-    | otherwise = (n, x, xs, rA)
+test :: (Eq a) => (a, [a], [a]) -> (a, [a], [a])
+test (x, [], rA) = (x, [], rA)
+test (x, xs, rA)
+    | x == (head xs) = test (x, (tail xs), (x : rA))
+    | otherwise = (x, xs, rA)
